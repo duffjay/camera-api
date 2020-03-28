75a6f1e9f55a0925d7186e60f6e1744c27810a60
Jay Duff <jay.duff@cfacorp.com>
2020-03-28 17:02:30 -0400

    multi

--- tensorflow_util.py
+++ tensorflow_util.py
@@ -2,6 +2,8 @@ import os
 import sys
 import time
 import cv2
+from PIL import Image
+
 import numpy as np
 import tensorflow as tf
 
@@ -16,6 +18,14 @@ sys.path.append(slim)
 from google.protobuf import text_format
 from object_detection.protos import string_int_label_map_pb2
 #import tflite_runtime.interpreter as tflite
+
+# Helper Function
+# load an image and resturn a numpy array
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
 
 def make_objects(boxes, class_ids_rev):
     '''
@@ -98,6 +108,127 @@ def get_tflite_attributes(interpreter):
     print ("Model Input Dimension: {}".format(model_input_dim))
     return model_image_dim, model_input_dim, output_details
 
+def get_tf_session(graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+            # Get handles to input and output tensors
+            ops = tf.get_default_graph().get_operations()
+            # print ("ALL model operations:", type(ops), len(ops), ops)
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            print ("tensor names:", type(all_tensor_names), len(all_tensor_names))
+            tensor_dict = {}
+            for key in [
+                'num_detections', 'detection_boxes', 'detection_scores',
+                'detection_classes', 'detection_masks'
+            ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_value = tf.get_default_graph().get_tensor_by_name(tensor_name)
+                    print (tensor_name, tensor_value)
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+            if 'detection_masks' in tensor_dict:
+                print ("*** detection mask in the tensor dict ***")
+                # The following processing is only for single image
+                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    detection_masks, detection_boxes, image.shape[1], image.shape[2])
+                detection_masks_reframed = tf.cast(
+                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                # Follow the convention by adding back the batch dimension
+                tensor_dict['detection_masks'] = tf.expand_dims(
+                    detection_masks_reframed, 0)
+            
+            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+    return sess, tensor_dict, image_tensor
+
+def send_image_to_tf_sess(image_np_expanded, sess, tensor_dict, image_tensor):
+    output_dict = sess.run(tensor_dict,feed_dict={image_tensor: image_np_expanded})
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+    output_dict['detection_classes'] = output_dict[
+        'detection_classes'][0].astype(np.int64)
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+    if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+
+# frozen graph
+# -- development only -- this should be discarded at some point
+def send_imagelist_to_frozen_graph(image_list, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      # print ("ALL model operations:", type(ops), len(ops), ops)
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      print ("tensor names:", type(all_tensor_names), len(all_tensor_names))
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_value = tf.get_default_graph().get_tensor_by_name(tensor_name)
+          print (tensor_name, tensor_value)
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+      if 'detection_masks' in tensor_dict:
+        print ("*** detection mask in the tensor dict ***")
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[1], image.shape[2])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+      print ("image_tensor:", image_tensor)
+        
+      # Run inference
+      print (" --- run the model ---")
+
+
+      for i,image_path in enumerate(image_list):
+        print (i, image_path)
+        image = Image.open(image_path)
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = load_image_into_numpy_array(image_path)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        start = time.perf_counter()
+        # -- run model
+        # output_dict = tensorflow_util.send_image_to_frozen_graph(image_np_expanded, detection_graph)
+        output_dict = sess.run(tensor_dict,feed_dict={image_tensor: image_np_expanded})
+        # all outputs are float32 numpy arrays, so convert types as appropriate
+        output_dict['num_detections'] = int(output_dict['num_detections'][0])
+        output_dict['detection_classes'] = output_dict[
+            'detection_classes'][0].astype(np.int64)
+        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+        if 'detection_masks' in output_dict:
+            output_dict['detection_masks'] = output_dict['detection_masks'][0]
+        # print ("Detection:", output_dict)
+        finish = time.perf_counter()
+        print (f'Finished in {round(finish - start, 2)} seconds(s)')
+
+
+  return i
+
 def send_image_to_model(preprocessed_image, interpreter, threshold):
     '''
     send the image into the model
@@ -121,6 +252,7 @@ def send_image_to_model(preprocessed_image, interpreter, threshold):
     # print ("DEBUG prob_data:", prob_data)
     prob_data = np.nan_to_num(prob_data)   # replace nan with 0
     bbox_data = np.nan_to_num(bbox_data)
+    
     reduction_index = np.argwhere((prob_data > threshold) & (prob_data <= 1.0))
     if reduction_index.size > 0:
         return bbox_data[reduction_index], class_data[reduction_index], prob_data[reduction_index]
--- test_tf_inference.py
+++ test_tf_inference.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
 
-import numpy
+import numpy as np
 import cv2
+from PIL import Image
 import tensorflow as tf
 
 import tensorflow_util
@@ -14,7 +16,46 @@ MODEL_PATH = os.path.join(PROJECT_DIR, "model/frozen_inference_graph.pb")
 LABEL_MAP = os.path.join(PROJECT_DIR, "model/mscoco_label_map.pbtxt")
 ANNOTATION_DIR = os.path.join(PROJECT_DIR, "annotation")
 
+# get a frozen graph
 detection_graph = tensorflow_util.get_detection_graph(MODEL_PATH)
-
 print("Detection Graph:", type(detection_graph))
-print  (detection_graph)
+
+# get images list
+dir_list = os.listdir(IMAGE_DIR)
+image_list = list()
+for f in dir_list:
+    full_path = os.path.join(IMAGE_DIR, f)
+    if os.path.isfile(full_path) and os.path.splitext(f)[1].lower() == '.jpg':
+        image_list.append(full_path)
+
+# limitations with the way we are displaying
+image_list = image_list[:1000]
+print ("Image Count:", len(image_list))
+print ("Sample:",image_list[0])
+
+# run all of the images through
+
+# i = tensorflow_util.send_image_to_frozen_graph(image_list, detection_graph)
+
+sess, tensor_dict, image_tensor = tensorflow_util.get_tf_session(detection_graph)
+
+for i,image_path in enumerate(image_list):
+    print (i, image_path)
+    image = Image.open(image_path)
+    # the array based representation of the image will be used later in order to prepare the
+    # result image with boxes and labels on it.
+    image_np = tensorflow_util.load_image_into_numpy_array(image_path)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Actual detection.
+    start = time.perf_counter()
+    # -- run model
+    output_dict = tensorflow_util.send_image_to_tf_sess(image_np_expanded, sess, tensor_dict, image_tensor)
+
+    # print ("Detection:", output_dict)
+    finish = time.perf_counter()
+    print (f'Finished in {round(finish - start, 2)} seconds(s)')
+    if i > 100:
+        break
+
+
