9c7ac64eb9c49d4c0247f6153e9a0ce91294b548
Jay Duff <duffjay@gmail.com>
2020-05-08 16:30:55 -0400

    auto correct images

--- camera_util.py
+++ camera_util.py
@@ -12,8 +12,52 @@ import time
 from PIL import Image
 
 
+def automatic_brightness_and_contrast(image, clip_hist_percent=25):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
+
 # DEPRECATED
-def get_camera_config(config, camera_num):
+def DEP_get_camera_config(config, camera_num):
     '''
     given the config JSON and a camera number, 
     return the config 
@@ -179,6 +223,9 @@ def extract_regions(config, image):
         dim = (width, height)
         # resize image
         resized_image = cv2.resize(regional_image, dim, interpolation = cv2.INTER_AREA)
+        #  auto correct (but not the raw full image - its already correct)
+        if i > 0:
+            resized_image, alpha, beta = automatic_brightness_and_contrast(resized_image, clip_hist_percent=25)
         # reshape to (1,480,640,3)
         np_image = resized_image.reshape((1,height,width,3))
 
---
+++ test_auto_correct_image.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+
+
+image_list = [
+    "/hsdata/jpeg_images/202005/15882870315-2-0-c.jpg",
+    "/hsdata/jpeg_images/202005/15882870411-2-0-g.jpg",
+    "/hsdata/jpeg_images/202005/15882869563-0-1-c.jpg",
+    "/hsdata/jpeg_images/202005/15882867256-0-1-c.jpg",
+    "/hsdata/jpeg_images/202005/15882864217-2-0-c.jpg",
+    "/hsdata/jpeg_images/202005/15882851210-2-0-g.jpg",
+    "/hsdata/jpeg_images/202005/15884420314-1-1-c.jpg",
+    "/hsdata/jpeg_images/202005/15884590286-0-1-c.jpg"
+    ]
+
+
+def automatic_brightness_and_contrast(image, clip_hist_percent=35):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
+
+# main
+
+for image_path in image_list:
+    image = cv2.imread(image_path)
+    auto_result, alpha, beta = automatic_brightness_and_contrast(image)
+    print('alpha', alpha)
+    print('beta', beta)
+    cv2.imshow('auto_result', auto_result)
+    cv2.imwrite('auto_result.png', auto_result)
+    cv2.imshow('image', image)
+    cv2.waitKey()
+
+
