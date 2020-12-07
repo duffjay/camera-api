58f0231a339c31402b9803c300df3500303a0969
Jay Duff <duffjay@gmail.com>
2020-12-07 18:54:30 -0500

    make ssd-usb drive a variable in notebooks

--- purge.sh
+++ purge.sh
@@ -1,3 +1,22 @@
-rm annotation/*.xml
 rm snapshot/*.xml
 rm snapshot/*.jpg
+#
+rm -r trash
+mkdir trash
+#
+rm -r stream
+mkdir stream
+mkdir stream/cam0
+mkdir stream/cam1
+mkdir stream/cam2
+mkdir stream/cam3
+mkdir stream/cam4
+mkdir stream/cam5
+mkdir stream/cam6
+#
+rm -r faces
+mkdir faces
+#
+rm *.log*
+
+
